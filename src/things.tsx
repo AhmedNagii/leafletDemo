f9db422be49bf1a1409283fccf7adc0a6f982f6d
Ahmed Nagi <ahmed.nagy@solve-it-soluitions.com>
2023-01-30 13:45:44 +0200

    basic example

---
+++ src/things.tsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+import { MapContainer, Marker, Popup, TileLayer, useMap, Polyline } from 'react-leaflet'
+import locations from "./data/locations.json"
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const sortedLocations = locations.sort((date1: any, date2: any) => date1.timestamp - date2.timestamp);
+
+
+  const linePoints: any = locations.map(loc => [loc.long, loc.lat])
+
+  return (
+
+
+    <MapContainer center={[30.06, 31.23]} zoom={10} scrollWheelZoom={true}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+
+      {/* {
+        locations.map(loc => (
+
+
+          <Marker key={loc.id}
+            position={[loc.lat, loc.long]}>
+
+            <Popup position={[loc.lat, loc.long]}>
+              <h2>{loc.timestamp}</h2>
+            </Popup>
+          </Marker>
+
+        ))
+
+
+      } */}
+      {
+        locations.map((point: any, i: number) => (
+          <Polyline key={i} positions={linePoints} ></Polyline>
+        ))
+      }
+
+
+    </MapContainer>
+
+  )
+}
+
+export default App
